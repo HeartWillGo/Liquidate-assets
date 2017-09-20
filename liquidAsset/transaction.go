41f7edb237d3a350d215d0699220a43f02a36e9b
certsblock <hellodaneel@gmail.com>
2017-09-20 10:46:05 +0800

    增加了各种索引

--- liquidAsset/transaction.go
+++ liquidAsset/transaction.go
@@ -80,36 +80,142 @@ func (t *SimpleChaincode) Transaction(stub shim.ChaincodeStubInterface, args []s
 		return shim.Error(err.Error())
 	}
 
-	NameIndexKey, err := stub.CreateCompositeKey("TransactionObject", []string{transaction.Fromid, transaction.Transactionid})
+	// 以下添加各种索引
+
+	stub.GetTxTimestamp()
+	value := []byte{0x00}
 
 
+	// Fromid~Transactionid
+	Fromid_Transactionid, err := stub.CreateCompositeKey("Fromid~Transactionid", []string{transaction.Fromid, transaction.Transactionid})
 	if err != nil {
 		return shim.Error(err.Error())
 	}
+	stub.PutState(Fromid_Transactionid, value)
 
-	value := []byte{0x00}
-	stub.PutState(NameIndexKey, value)
+	// Fromid~Productid~Transactionid
+	Fromid_Productid_Transactionid, err := stub.CreateCompositeKey("Fromid~Productid~Transactionid", []string{transaction.Fromid,
+																	transaction.Productid, transaction.Transactionid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Fromid_Productid_Transactionid, value)
+
+	// Fromid~Organizationid~Transactionid
+	Fromid_Organizationid_Transactionid, err := stub.CreateCompositeKey("Fromid~Organizationid~Transactionid", []string{transaction.Fromid, transaction.Organizationid, transaction.Transactionid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Fromid_Organizationid_Transactionid, value)
+
+	// Toid~Transactionid
+	Toid_Transactionid, err := stub.CreateCompositeKey("Toid~Transactionid", []string{transaction.Toid, transaction.Transactionid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Toid_Transactionid, value)
+
+	// Toid~Productid~Transactionid
+	Toid_Productid_Transactionid, err := stub.CreateCompositeKey("Toid~Productid~Transactionid", []string{transaction.Toid, transaction.Productid, transaction.Transactionid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Toid_Productid_Transactionid, value)
+
+	// Toid~Organizationid~Transactionid
+	Toid_Organizationid_Transactionid, err := stub.CreateCompositeKey("Toid~Organizationid~Transactionid", []string{transaction.Toid, transaction.Organizationid, transaction.Transactionid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Toid_Organizationid_Transactionid, value)
+
+	// Productid~Transactionid
+	Productid_Transactionid, err := stub.CreateCompositeKey("Productid~Transactionid", []string{transaction.Productid, transaction.Transactionid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Productid_Transactionid, value)
+	// Productid~Fromid
+	Productid_Fromid, err := stub.CreateCompositeKey("Productid~Fromid", []string{transaction.Productid, transaction.Fromid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Productid_Fromid, value)
+	// Productid~Toid
+	Productid_Toid, err := stub.CreateCompositeKey("Productid~Toid", []string{transaction.Productid, transaction.Toid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Productid_Toid, value)
+
+	// Organizationid~Transactionid
+	Organizationid_Transactionid, err := stub.CreateCompositeKey("Organizationid~Transactionid", []string{transaction.Organizationid, transaction.Transactionid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Organizationid_Transactionid, value)
+
+	// Organizationid~Productid
+	Organizationid_Productid, err := stub.CreateCompositeKey("Organizationid~Transactionid", []string{transaction.Organizationid, transaction.Productid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Organizationid_Productid, value)
+
+	// Organizationid~Fromid
+	Organizationid_Fromid, err := stub.CreateCompositeKey("Organizationid~Fromid", []string{transaction.Organizationid, transaction.Fromid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Organizationid_Fromid,value)
+
+	// Organizationid~Fromid~Productid
+	Organizationid_Fromid_Productid, err := stub.CreateCompositeKey("Organizationid~Fromid~Productid", []string{transaction.Organizationid,
+																	transaction.Fromid, transaction.Productid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Organizationid_Fromid_Productid, value)
+
+	// Organizationid~Toid
+	Organizationid_Toid, err := stub.CreateCompositeKey("Organizationid~Toid", []string{transaction.Organizationid, transaction.Toid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Organizationid_Toid, value)
+
+	// Organizationid~Toid~Productid
+	Organizationid_Toid_Productid, err := stub.CreateCompositeKey("Organizationid~Toid~Productid", []string{transaction.Organizationid,
+																	transaction.Toid, transaction.Productid})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	stub.PutState(Organizationid_Toid_Productid,value)
+
+
 
 // ================
 	return shim.Success(nil)
 }
 
-// 得到某一用户的交易
+// 得到某一用户的所有交易
 func  (t *SimpleChaincode) getTransactionByUserID(stub shim.ChaincodeStubInterface, args []string) pb.Response  {
-	fmt.Println("0x03 进入了信息时代")
+	fmt.Println("0x03 根据FromID查询交易")
 	if len(args) < 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
 
-	getIndex := args[1:]
-	fmt.Println("we get Index", getIndex)
-	// Query the TransactionObject index by color
+	Fromid := args[1:]
+	fmt.Println("we get Fromid", Fromid)
+	// Query the TransactionObject index by FromID
 	// This will execute a key range query on all keys starting with 'color'
-	transactionResultsIterator, err := stub.GetStateByPartialCompositeKey("TransactionObject", []string{"0","0"})
+	transactionFromidResultsIterator, err := stub.GetStateByPartialCompositeKey("Fromid~Transactionid", Fromid)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-	defer transactionResultsIterator.Close()
+	defer transactionFromidResultsIterator.Close()
+
+
 
 	// Iterate through result set and for each marble found, transfer to newOwner
 	bArrayMemberAlreadyWritten := false
@@ -117,24 +223,37 @@ func  (t *SimpleChaincode) getTransactionByUserID(stub shim.ChaincodeStubInterfa
 	buffer.WriteString("[")
 	for transactionResultsIterator.HasNext() {
 		// Note that we don't get the value (2nd return variable), we'll just get the marble name from the composite key
-		queryResponse, err := transactionResultsIterator.Next()
+		queryResponse, err := transactionFromidResultsIterator.Next()
 		if err != nil {
 			return shim.Error(err.Error())
 		}
 		if bArrayMemberAlreadyWritten == true {
 			buffer.WriteString(",")
 		}
+
+		objectType, compositeKeyParts, err := stub.SplitCompositeKey(queryResponse.Key)
+		if err != nil {
+			return shim.Error("we cannot splitcompositekey")
+		}
+		if objectType != "Fromid" {
+			return shim.Error("object is not we want %s"+ Fromid[0])
+		}
+		transactionid := compositeKeyParts[len(compositeKeyParts)-1]
+
+		transactionBytes,err := stub.GetState(transactionid)
+		if err != nil {
+			return shim.Error("the transactionid is not put in the ledger")
+		}
 		buffer.WriteString("{\"Key\":")
 		buffer.WriteString("\"")
-		buffer.WriteString(queryResponse.Key)
-		buffer.WriteString("\"")
+		buffer.WriteString(Fromid[0])
 
 		buffer.WriteString(", \"Record\":")
 		// Record is a JSON object, so we write as-is
-		fmt.Println("queryUserID", queryResponse)
+		fmt.Println("what the fuck", string(transactionBytes))
+		buffer.WriteString(string(transactionBytes))
 		buffer.WriteString("}")
 		bArrayMemberAlreadyWritten = true
-
 
 	}
 	buffer.WriteString("}")
